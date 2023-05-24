c0bc3487d7da15cce1a6c7eb4a0ea62ffec36f69
Szymon Brodawka <szymbrod@gmail.com>
2023-05-25 00:11:08 +0200

    Deleting variable in getMonthDifference function

--- js/calculateDate/getMonthDifference.js
+++ js/calculateDate/getMonthDifference.js
@@ -1,10 +1,9 @@
 import { getCurrentDate } from "./getCurrentDate.js";
 
 export const getMonthDifference = (month) => {
-    const MONTHS_IN_YEAR = 12;
     const currentMonth = getCurrentDate().getMonth() + 1;
     
-    const monthDifference = (currentMonth - month) % MONTHS_IN_YEAR;
+    const monthDifference = currentMonth - month;
     
     return monthDifference;
 }
