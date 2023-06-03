42f83e74068f1d372b016655368b9cc9f8f0c73f
Szymon Brodawka <szymbrod@gmail.com>
2023-06-03 15:34:45 +0200

    Fixing a bug with isLeapYear function showing that a year 21 was a leap year

--- js/calculateDate/isLeapYear.js
+++ js/calculateDate/isLeapYear.js
@@ -2,7 +2,7 @@ export const isLeapYear = (year) => {
     let isLeapYear = false;
     
     //If a year is a date that starts a new century(1800, 1900, 2000 etc.) and is dividable by 400 then the year is a leap one
-    if(year % 100 === 0 && 400) {
+    if(year % 100 === 0 && year % 400 === 0) {
         isLeapYear = true;
     }
 
